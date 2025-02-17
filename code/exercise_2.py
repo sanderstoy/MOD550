403554f364d93f6010930a802744089c2733eb7a
sanderstoy <sterrii@icloud.com>
2025-02-17 19:25:58 +0100

    fyf

--- code/exercise_2.py
+++ code/exercise_2.py
@@ -186,7 +186,7 @@ def neural_network():
     loss_nn = []  # Loss list
     counter = []  # Counter list
     predictions_nn = {} # Predictions dictionary
-    epochs = 601    # Number of epochs
+    epochs = 600    # Number of epochs
     # Training loop
     for epoch in range(epochs):
         model.train()   # Set model to training mode
@@ -210,53 +210,143 @@ def neural_network():
     x_test_np = x_test.numpy()      # Convert to NumPy for visualization
     y_test_np = y_test.numpy()      # Convert to NumPy for visualization
 
-    fig, ax = plt.subplots(1,4)
+    fig, ax = plt.subplots(2,2)
     # plots the evolution of the regression line every 100 epochs.
-    for epoch in [0, 100, 200, 300, 400, 500]:
+    for epoch in [0, 50, 100, 200, 300, 400]:
         if epoch in predictions_nn:
             sort_idx = np.argsort(x_train.numpy().flatten())    
             x_sorted = x_train.numpy()[sort_idx]    
             y_sorted = predictions_nn[epoch][sort_idx]
-            ax[1].scatter(x_train.numpy(), y_train.numpy(), alpha=0.5, label="Training Data")
-            ax[1].plot(x_sorted, y_sorted, label=f'Epoch {epoch}')
-            ax[1].set_xlabel("X (Feature)")
-            ax[1].set_ylabel("Y (Predicted)")
-            ax[1].legend()
-            ax[1].set_title("Neural Network Regression")
-            ax[1].grid()
+            ax[0,1].scatter(x_train.numpy(), y_train.numpy(), alpha=0.5, label="Training Data")
+            ax[0,1].plot(x_sorted, y_sorted, label=f'Epoch {epoch}')
+            ax[0,1].set_xlabel("X (Feature)")
+            ax[0,1].set_ylabel("Y (Predicted)")
+            ax[0,1].legend(loc='upper left', bbox_to_anchor=(1, 1))
+            ax[0,1].set_title("Neural Network Regression")
+            ax[0,1].grid()
     # plots the error with respect to the truth
-    ax[2].plot(counter, error_nn, label="Training Loss")
-    ax[2].set_xlabel("Epoch")
-    ax[2].set_ylabel("error")
-    ax[2].legend()
-    ax[2].set_title("Error with respect to the truth")
-    ax[2].grid()
-    ax[3].plot(counter, loss_nn, label="Test Loss")
-    ax[3].set_xlabel("Epoch")
-    ax[3].set_ylabel("Loss")
-    ax[3].legend()
-    ax[3].set_title("Loss")
-    ax[3].grid()
+    ax[1,0].plot(counter, error_nn, label="Error")
+    ax[1,0].set_xlabel("Epoch")
+    ax[1,0].set_ylabel("error")
+    ax[1,0].legend()
+    ax[1,0].set_title("Error with respect to the truth")
+    ax[1,0].grid()
+    ax[1,1].plot(counter, loss_nn, label="Loss")
+    ax[1,1].set_xlabel("Epoch")
+    ax[1,1].set_ylabel("Loss")
+    ax[1,1].legend()
+    ax[1,1].set_title("Loss")
+    ax[1,1].grid()
 
     # Sort the predictions for visualization
     # Final state plot
     sort_idx = np.argsort(x_test_np[:, 0])
-    x_sorted, y_pred_sorted = x_test_np[sort_idx], y_pred_np[sort_idx]
-    ax[0].scatter(x_test_np, y_test_np, label="Test Data", alpha=0.7)
-    ax[0].plot(x_sorted, y_pred_sorted, color='red', label="NN Regression Line")
-    ax[0].set_xlabel("X (Feature)")
-    ax[0].set_ylabel("Y (predicted)")
-    ax[0].legend()
-    ax[0].set_title("Neural Network Regression")
-    ax[0].grid()
+    x_sorted_, y_pred_sorted = x_test_np[sort_idx], y_pred_np[sort_idx]
+    ax[0,0].scatter(x_test_np, y_test_np, label="Test Data", alpha=0.7)
+    ax[0,0].plot(x_sorted_, y_pred_sorted, color='red', label=f"Epoch {epochs}")
+    ax[0,0].set_xlabel("X (Feature)")
+    ax[0,0].set_ylabel("Y (predicted)")
+    ax[0,0].legend()
+    ax[0,0].set_title("Neural Network Regression")
+    ax[0,0].grid()
     print(f'Task completed: Neural network')
     plt.show()
 
 
+class PINN_model(nn.Module):
+    def __init__(self):
+        super(PINN_model, self).__init__()
+        self.fc1 = nn.Linear(1, 50)
+        self.fc2 = nn.Linear(50, 50)
+        self.fc3 = nn.Linear(50, 1)
+
+    def forward(self, x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        return self.fc3(x)
+    
+
+def test():
+    x_train, x_test, y_train, y_test, y_true_train, y_true_test = train_test()  # Train-test split
+    pinn = PINN_model()
+    t_physics = torch.linspace(0, 1, 30).view(-1, 1).requires_grad_(True)
+    criterion = nn.MSELoss()  # Loss function
+    optimiser = torch.optim.Adam(pinn.parameters(), lr=1e-3)  # Learning rate: 0.001
+
+    loss_list = []
+    error_pinn = []
+    counter = []
+    predictions_pinn = {}
+    epoch = 4000
+    for i in range(epoch):
+        pinn.train()
+        optimiser.zero_grad()
+        y_pred = pinn(x_train)
+        loss = criterion(y_pred, y_train)
+        u = pinn(t_physics)
+        dudx = torch.autograd.grad(u, t_physics, torch.ones_like(u), create_graph=True)[0]
+        d2u_dt2 = torch.autograd.grad(dudx, t_physics, torch.ones_like(dudx), create_graph=True)[0]
+        loss_pinn = torch.mean((d2u_dt2 + u) ** 2) 
+        total_loss = loss + loss_pinn
+        total_loss.backward()
+        optimiser.step()
+        if i % 100 == 0:
+            pinn.eval()
+            counter.append(i)
+            predictions_pinn[i] = y_pred.detach().numpy()
+            with torch.no_grad():
+                y_pred_pinn = pinn(x_test)
+                error_pinn.append(calculate_error(y_true_test, y_pred_pinn))
+                loss_list.append(criterion(y_pred_pinn, y_test).item())
+        
+    y_pred_np_pinn = y_pred_pinn.numpy()   # Convert to NumPy for visualization
+    x_test_np_pinn = x_test.numpy()        # Convert to NumPy for visualization
+    y_test_np_pinn = y_test.numpy()        # Convert to NumPy for visualization
+        
+    sort_idx = np.argsort(x_test_np_pinn[:, 0])
+    x_sorted_pinn, y_pred_sorted_pinn = x_test_np_pinn[sort_idx], y_pred_np_pinn[sort_idx]
+    
+    fix, ax = plt.subplots(2, 2)
+    ax[0,0].scatter(x_test_np_pinn, y_test_np_pinn, label="Test Data", alpha=0.7)
+    ax[0,0].plot(x_sorted_pinn, y_pred_sorted_pinn, color='red', label=f"Epoch {epoch}")
+    ax[0,0].set_xlabel("X")
+    ax[0,0].set_ylabel("Y")
+    ax[0,0].legend()
+    ax[0,0].set_title("PINN")
+    ax[0,0].grid()
+
+    ax[1,1].plot(counter, error_pinn, label="Error")
+    ax[1,1].set_xlabel("Epoch")
+    ax[1,1].set_ylabel("Error")
+    ax[1,1].legend()
+    ax[1,1].set_title("Error with respect to the truth")
+    ax[1,1].grid()
+
+    ax[1,0].plot(counter, loss_list, label="Loss")
+    ax[1,0].set_xlabel("Epoch")
+    ax[1,0].set_ylabel("Loss")
+    ax[1,0].legend()
+    ax[1,0].set_title("Loss")
+    ax[1,0].grid()
+
+    for epoch in [0, 300, 600, 900, 1200, 2000, 3000]:
+        if epoch in predictions_pinn:
+            sort_idx = np.argsort(x_train.numpy().flatten())    
+            x_sorted_pinn_ = x_train.numpy()[sort_idx]    
+            y_sorted_pinn_ = predictions_pinn[epoch][sort_idx]
+            ax[0,1].scatter(x_train.numpy(), y_train.numpy(), alpha=0.5, label="Training Data")
+            ax[0,1].plot(x_sorted_pinn_, y_sorted_pinn_, label=f'Epoch {epoch}')
+            ax[0,1].set_xlabel("X (Feature)")
+            ax[0,1].set_ylabel("Y (Predicted)")
+            ax[0,1].legend(loc='upper left', bbox_to_anchor=(1, 1))
+            ax[0,1].set_title("Neural Network Regression")
+            ax[0,1].grid()
+
+    plt.show()
 
 linear_regression()
 neural_network()
-
+test()
 
 
 
